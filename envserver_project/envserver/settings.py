cbc2692ee51ee91473f3f2b0793eefafd5ecf7ff
Josh <josh@halfnibble.com>
2016-04-24 09:30:36 -0700

    Setup uwsgi server.

--- envserver_project/envserver/settings.py
+++ envserver_project/envserver/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from django.core.exceptions import ImproperlyConfigure
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # REST Framework
 REST_FRAMEWORK = {
